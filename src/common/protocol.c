2495a5357046fd51e00560b6d456499facc09ffb
guilherme <mnbvcasadg@gmail.com>
2026-09-17 20:25:22 -0400

    feat(protocol): serializacao de header e payloads JOIN/ACK/ERROR
    Define protocol.h com pl_header de 104 bytes (NodeID/TransactionID como
    bytes, checksum e timestamp) e as structs de JOIN/ACK/ERROR, e implementa
    encode/decode big-endian explicitos (pack/unpack) mais send_message/
    recv_message sobre send_all/recv_all para o Checkpoint 1.

--- src/common/protocol.c
+++ src/common/protocol.c
@@ -2,19 +2,17 @@
 #include <netinet/in.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 
-#include "network.h"
+#include "../../include/common/network.h"
+#include "../../include/common/protocol.h"
 
-// TEMP
-static const int FILE_NAME_MAX = 25;
 
-// TEMP
-#include <stdint.h>
 
-uint64_t my_ntohll(uint64_t val) {
+static uint64_t my_ntohll(uint64_t val) {
     #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
         return val;
     #else
@@ -23,174 +21,214 @@ uint64_t my_ntohll(uint64_t val) {
     #endif
 }
 
+static const int32_t payload_sizes[MSG_TYPE_MAX] = {
+	[JOIN] = 39,
+	[LEAVE] = 32,
+	[ERROR] = 68,
+	[ACK] = 32,
 
-
-#define HEADER_SIZE 104
-
-enum message_type{
-	JOIN,
-	LEAVE,
-	LOOKUP,
-	STORE,
-	DOWN,
-	LOAD_REQ,
-	DOWNLOAD_REP,
-	PREPARE,
-	COMMIT,
-	ABORT,
-	HEARTBEAT,
-	GOSSIP,
-	ELECTION,
-	OK,
-	COORDINATOR,
-	SNAPSHOT,
-	STATE_TRANSFER,
-	ACK,
-	ERROR,
 };
 
-typedef enum compression_type{
-	LZ4,
-}compress_type;
+int pack_join(const join_t* in_st, char* out_msg){
+	char* pt = out_msg;
+	uint32_t temp_32;
+	uint16_t temp_16;
 
-typedef enum node_type{
-	PEER,
-	SUPERPEER,
-}node_type;
+	memcpy(pt, in_st->node_id, sizeof(uint8_t)*32);
+	pt += sizeof(uint8_t)*32;
+	
+	temp_32 = htonl(in_st->ipv4);
+	memcpy(pt, &temp_32, sizeof(uint32_t));
+	pt += sizeof(uint32_t);
 
-typedef enum metadata_status{
-	ATIVO,
-	REPLICANDO,
-	REMOVIDO,
-}mtdata_status;
+	temp_16 = htons(in_st->port);
+	memcpy(pt, &temp_16, sizeof(uint16_t));
+	pt += sizeof(uint16_t);
 
-//TODO:  types of each attribute to be decided
-typedef struct payloadHeader{
-	uint32_t protocol_ver;
-	uint32_t msg_type;
-	uint8_t src_node[32];
-	uint8_t dst_node[32];
-	uint8_t trsc_id[16];
-	uint64_t time;
-	uint32_t pl_size;
-	uint32_t checksun;
-}pl_header;
-
-typedef struct file_metadata{
-	uint8_t obj_id[32];
-	char* file_name;
-	uint64_t version;
-	uint64_t size;
-	compress_type compression;
-	uint8_t owner_peer[32];
-	uint8_t* replica_peers[32];
-	uint32_t chunk_count;
-	uint8_t* chunk_hash[32];
-	uint64_t upload_date;
-	uint64_t last_access;
-	uint64_t download_counter;
-	mtdata_status status;
-}fl_mtdata;
-
-typedef struct message_struct{
-	pl_header header;
-	void* payload;
-	uint8_t status;
-}msg_t;
-
-typedef struct joinPayload{
-	uint8_t node_id[32];
-	uint32_t ipv4;
-	uint16_t port;
-	uint8_t node_type;
-}join_t;
-
-typedef struct ackPayload{
-	uint8_t node_id[32];
-}ack_t;
-
-typedef struct errorPayload{
-	uint32_t code;
-	uint8_t reason[64];
-}error_t;
+	memcpy(pt, &in_st->node_type, sizeof(uint8_t));
+	pt += sizeof(uint8_t);	
+	return 0;
+}
 
 
-/* WRONG
-void pack_message(const char* msg_header, const char* payload, int payload_size, char* out_msg){
-	const int message_size = payload_size + HEADER_SIZE;
-	if(!msg_header || !payload || !out_msg){
-		if(!payload)
-			fprintf(stderr, "pack_message # null payload");
-		if(!msg_header)
-			fprintf(stderr, "pack_message # null message header");
-		if(!out_msg)
-			fprintf(stderr, "pack_message # null message buffer");
-		return;
-	}
+int unpack_join(join_t* out_st, const char* in_msg){
+	const char* pt = in_msg;
+	uint32_t temp_32;
+	uint16_t temp_16;
 
-	void* msg_pointer = out_msg; 
-	int i = 0;
-	int remaining = message_size;
-	while(remaining == 0){
-		if(remaining >= 4){
-			uint32_t buffer32 = htonl(*(uint32_t*)out_msg);
-			memcpy(msg_pointer, &buffer32, sizeof(uint32_t));
-			msg_pointer += 4;
-			remaining -= 4;
-		}
-		else if(remaining >= 2){
-			uint16_t buffer16 = htonl(*(uint16_t*)out_msg);
-			memcpy(msg_pointer, &buffer16, sizeof(uint16_t));
-			msg_pointer += 2;
-			remaining -= 2;
+	memcpy(out_st->node_id, pt, sizeof(uint8_t)*32);
+	pt += sizeof(uint8_t)*32;
+	
+	memcpy(&temp_32, pt, sizeof(uint32_t));
+	out_st->ipv4 = ntohl(temp_32);
+	pt += sizeof(uint32_t);
 
-		}
-		else{
-			remaining = 0;
-		}
+	memcpy(&temp_16, pt, sizeof(uint16_t));
+	out_st->port = ntohs(temp_16);
+	pt += sizeof(uint16_t);
 
-	}
+	memcpy(&out_st->node_type, pt, sizeof(uint8_t));
+	pt += sizeof(uint8_t);	
+	return 0;
+}
 
-}*/
+int pack_ack(const ack_t* in_st, char* out_msg){
+	char* pt = out_msg;
 
-int unpack_header(pl_header* out_header, const char* header_buf){
-	const char* pt = header_buf;
+	memcpy(pt, in_st->node_id, sizeof(uint8_t)*32);
+	pt += sizeof(uint8_t)*32;
+	
+	return 0;
+}
+
+int unpack_ack(ack_t* out_st, const char* in_msg){
+	const char* pt = in_msg;
+
+	memcpy(out_st->node_id, pt, sizeof(uint8_t)*32);
+	pt += sizeof(uint8_t)*32;
+
+	return 0;
+}
+
+
+int pack_error(const error_t* in_st, char* out_msg){
+	char* pt = out_msg;
+	uint32_t temp_32 = htonl(in_st->code);
+	memcpy(pt, &temp_32, sizeof(uint32_t));
+	pt += sizeof(uint32_t);
+
+	memcpy(pt, in_st->reason, sizeof(uint8_t)*64);
+	pt += sizeof(uint8_t)*64;
+	
+	return 0;
+}
+
+int unpack_error(error_t* out_st, const char* in_msg){
+	const char* pt = in_msg;
+	uint32_t temp_32;
+	memcpy(&temp_32, pt, sizeof(uint32_t));
+	out_st->code = ntohl(temp_32);
+	pt += sizeof(uint32_t);
+
+	memcpy(out_st->reason, pt, sizeof(uint8_t)*64);
+	pt += sizeof(uint8_t)*64;
+
+	return 0;
+}
+
+int unpack_header(pl_header* out_st, const char* in_msg){
+	const char* pt = in_msg;
 	uint32_t temp_32;
 	uint64_t temp_64;
 	memcpy(&temp_32, pt, sizeof(uint32_t));
-	temp_32 = ntohl(temp_32);
-	out_header->protocol_ver = temp_32;
+	out_st->protocol_ver = ntohl(temp_32);
 	pt+=sizeof(uint32_t);
 	memcpy(&temp_32, pt, sizeof(uint32_t));
-	temp_32 = ntohl(temp_32);
-	out_header->msg_type = temp_32;
+	out_st->msg_type = ntohl(temp_32);
 	pt+=sizeof(uint32_t);
 
-	memcpy(out_header->src_node, pt, sizeof(uint8_t)*32);
+	memcpy(out_st->src_node, pt, sizeof(uint8_t)*32);
 	pt+=(sizeof(uint8_t)*32);
-	memcpy(out_header->dst_node, pt, sizeof(uint8_t)*32);
+	memcpy(out_st->dst_node, pt, sizeof(uint8_t)*32);
 	pt+=(sizeof(uint8_t)*32);
-	memcpy(out_header->trsc_id, pt, sizeof(uint8_t)*16);
+	memcpy(out_st->trsc_id, pt, sizeof(uint8_t)*16);
 	pt+=(sizeof(uint8_t)*16);
 
 	memcpy(&temp_64, pt, sizeof(uint64_t));
-	temp_64 = my_ntohll(temp_64);
-	out_header->time = temp_64;
+	out_st->time = my_ntohll(temp_64);
 	pt+=sizeof(uint64_t);
 	memcpy(&temp_32, pt, sizeof(uint32_t));
-	temp_32 = ntohl(temp_32);
-	out_header->pl_size = temp_32;
+	out_st->pl_size = ntohl(temp_32);
 	pt+=sizeof(uint32_t);
 	memcpy(&temp_32, pt, sizeof(uint32_t));
-	temp_32 = ntohl(temp_32);
-	out_header->checksun = temp_32;
+	out_st->checksum = ntohl(temp_32);
 	pt+=sizeof(uint32_t);
 	
 	return 0;
 }
 
+int pack_header(const pl_header* in_st, char* out_st){
+	char* pt = out_st;
+	uint32_t temp_32;
+	uint64_t temp_64;
+	temp_32 = htonl(in_st->protocol_ver);
+	memcpy(pt, &temp_32, sizeof(uint32_t));
+	pt+=sizeof(uint32_t);
 
-msg_t recv_message(int fd){
+	temp_32 = htonl(in_st->msg_type);
+	memcpy(pt, &temp_32, sizeof(uint32_t));
+	pt+=sizeof(uint32_t);
+
+	memcpy(pt, in_st->src_node, sizeof(uint8_t)*32);
+	pt+=(sizeof(uint8_t)*32);
+	memcpy(pt, in_st->dst_node, sizeof(uint8_t)*32);
+	pt+=(sizeof(uint8_t)*32);
+	memcpy(pt, in_st->trsc_id, sizeof(uint8_t)*16);
+	pt+=(sizeof(uint8_t)*16);
+
+	temp_64 = my_ntohll(in_st->time);
+	memcpy(pt, &temp_64, sizeof(uint64_t));
+	pt+=sizeof(uint64_t);
+	temp_32 = htonl(in_st->pl_size);
+	memcpy(pt, &temp_32, sizeof(uint32_t));
+	pt+=sizeof(uint32_t);
+	temp_32 = htonl(in_st->checksum);
+	memcpy(pt, &temp_32, sizeof(uint32_t));
+	pt+=sizeof(uint32_t);
+	
+	return 0;
+
+}
+
+int send_message(int fd, char* out_msg_buffer, const void* msg_payload, const pl_header* msg_header){
+		
+	int status;
+	char* buffer_pointer = out_msg_buffer;
+	uint32_t message_type = msg_header->msg_type;
+	uint32_t payload_size = msg_header->pl_size;
+
+	pack_header(msg_header, buffer_pointer);
+	buffer_pointer += HEADER_SIZE;
+
+	if(message_type >= MSG_TYPE_MAX){
+		fprintf(stderr, "recv_message # corrupted header");
+		status = NET_ERROR; // TODO: add more error status types for logging
+		return status;
+	}
+
+	if(message_type != DOWNLOAD_REP && message_type != DOWNLOAD_REQ && message_type != GOSSIP && message_type != STATE_TRANSFER && message_type != SNAPSHOT){
+		if(payload_size > MAX_CONTROL_PAYLOAD_SZ || payload_sizes[message_type] != payload_size){
+			fprintf(stderr, "recv_message # corrupted header");
+			status = NET_ERROR; 
+			return status;
+		}
+	
+		switch(message_type){
+			case JOIN: {
+				pack_join(msg_payload, buffer_pointer);
+				break;
+			}
+			case ACK: {
+				pack_ack(msg_payload, buffer_pointer); 
+				break;
+			}
+			case ERROR: {
+				pack_error(msg_payload, buffer_pointer);
+				break;
+			}
+
+			default:
+				return NET_ERROR; 
+		}
+		if((status = send_all(fd, out_msg_buffer ,HEADER_SIZE + payload_size)) != NET_OK)
+			return status;
+
+
+	}
+	return NET_OK;
+}
+
+msg_t recv_message(int fd, char* in_msg_buffer, void* struct_payload){
 
 	char header_buffer[HEADER_SIZE]; 
 
@@ -198,18 +236,56 @@ msg_t recv_message(int fd){
 	
 	if((status = recv_all(fd, header_buffer, HEADER_SIZE)) != NET_OK)
 		return (msg_t){{0}, NULL, status};
-	
-	// TODO: unpack
 
 	pl_header msg_header;
 
 	unpack_header(&msg_header, header_buffer);
 	
+	uint32_t payload_size = msg_header.pl_size;
+	uint32_t message_type = msg_header.msg_type;
+
+	if(message_type >= MSG_TYPE_MAX){
+		fprintf(stderr, "recv_message # corrupted header");
+		status = NET_ERROR; // TODO: add more error status types for logging
+		return (msg_t){{0}, NULL, status};
+	}
+
+	if(message_type != DOWNLOAD_REP && message_type != DOWNLOAD_REQ &&  message_type != GOSSIP && message_type != STATE_TRANSFER && message_type != SNAPSHOT){
+		if(payload_size > MAX_CONTROL_PAYLOAD_SZ || payload_sizes[message_type] != payload_size){
+			fprintf(stderr, "recv_message # corrupted header");
+			status = NET_ERROR; 
+			return (msg_t){{0}, NULL, status};
+		}
+		if((status = recv_all(fd, in_msg_buffer, payload_size))!= NET_OK)
+			return (msg_t){{0}, NULL, status};
 	
-	// TODO: get and unpack payload 
-	
-		
-	return (msg_t){msg_header, NULL, NET_OK}; 
+		switch(message_type){
+			case JOIN: {
+				join_t new_st;
+				unpack_join(&new_st, in_msg_buffer);
+				memcpy(struct_payload, &new_st, sizeof(join_t));
+				break;
+			}
+			case ACK: {
+				ack_t new_st;
+				unpack_ack(&new_st, in_msg_buffer);
+				memcpy(struct_payload, &new_st, sizeof(ack_t));
+				break;
+			}
+			case ERROR: {
+				error_t new_st;
+				unpack_error(&new_st, in_msg_buffer);
+				memcpy(struct_payload, &new_st, sizeof(error_t));
+				return (msg_t){msg_header, struct_payload, NET_OK}; 
+				break;
+			}
+
+			default:
+				return (msg_t){msg_header, NULL, NET_OK}; 
+		}
+
+	}
+	return (msg_t){msg_header, struct_payload, NET_OK}; 
 
 }
 
